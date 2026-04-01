2b07bfeea0a92fd555a7fac1a6176d8061baf811
Aditya Tawde <adityatawde9699@gmail.com>
2026-04-01 06:23:25 +0000

    a**+

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
 // Set the base URL for your Django backend
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://queuebitebackend.vercel.app//api';
 
 const api = axios.create({
   baseURL: API_URL,
